c172a08f4bf0f8ab737913a661017ec2af375986
bbdev09 <55377115+bbdev09@users.noreply.github.com>
2020-08-26 20:40:48 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        wc_color: "#46A2F6",
+        body: "#F4F4F4",
+        hoverColor: "#F9FAFB"
+      },
+      screens: {
+        mobile: {
+          max: "480px"
+        },
+        tablet: {
+          max: "640px"
+        },
+        laptopp: {
+          max: "768px"
+        },
+        desktop: {
+          max: "1024px"
+        },
+        bigscreen: {
+          max: "1280px"
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
